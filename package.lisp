fe3a4304489e7eceb8f4711f48f3048a99786947
inaimathi <leo.zovic@gmail.com>
2018-02-22 13:24:30 -0500

    Add generic function/method introspection basics

--- package.lisp
+++ package.lisp
@@ -1,17 +1,27 @@
 ;;;; package.lisp
 
-(defpackage #:cl-mop 
-  (:use #:cl)	    
-  (:shadowing-import-from 
-   #+openmcl-native-threads #:ccl 
-   #+cmu #:pcl 
-   #+sbcl #:sb-pcl 
-   #+lispworks #:hcl 
-   #+allegro #:mop 
-   #+clisp #:clos 
+(defpackage #:cl-mop
+  (:use #:cl)
+  (:shadowing-import-from
+   #+openmcl-native-threads #:ccl
+   #+cmu #:pcl
+   #+sbcl #:sb-pcl
+   #+lispworks #:hcl
+   #+allegro #:mop
+   #+clisp #:clos
    #:class-slots #:slot-definition-name)
+  (:shadowing-import-from
+   #+sbcl #:sb-mop
+   #+clisp #:clos
+   #+openmcl-native-threads #:ccl
+
+   #:generic-function-methods
+   #:method-specializers
+   #:eql-specializer
+   #:eql-specializer-object)
   (:export #:slot-names #:map-slots
 	   #:to-alist
 	   #:shallow-copy #:deep-copy
-	   #:class-slots #:slot-definition-name))
+	   #:class-slots #:slot-definition-name
 
+           #:methods-of #:specializers-of #:specializer-object #:specializer-objects-of))
